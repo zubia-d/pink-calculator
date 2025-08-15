c055e7ba764d3eb89917f371c222e2f56ec68a2a
zubia-d <dahrizubia222@gmail.com>
2025-08-15 18:51:34 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let display = document.getElementById("display");
+
+function appendValue(value){
+    display.value += value;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch{
+        display.value = "Error";
+    }
+}
